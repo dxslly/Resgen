84b2c209b89648ce686d5f2ac4c2c159e724d076
David Samuelson <hello@dsamuelson.com>
2016-09-05 23:43:27 -0700

    Renamed old RTool classes to Resgen

---
+++ Run/Pug/Resgen/Resgen.cs
@@ -0,0 +1,21 @@
+﻿using Run.Pug.Diagnostics;
+using Run.Pug.Patterns;
+using Run.Pug.Util;
+
+namespace Run.Pug.Resgen
+{
+    public class Resgen
+    {
+        private const string CSHARP_FILE_EXTENSION = ".cs";
+
+        public static void GenerateClass(IClassGenerator generator, Config config)
+        {
+            string className = config.GetCustomClassName(generator).Or(generator.DefaultClassName);
+            string fileContents = generator.GenerateClass(className);
+
+            string directory = FileUtil.GetSafeDirectoryPath(config.GeneratePath);
+            string path = directory + className + CSHARP_FILE_EXTENSION;
+            FileUtil.WriteFile(path, fileContents);
+        }
+    }
+}
---
+++ Run/Pug/Resgen/Unity/Editor/ResgenWindow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Run.Pug.Util;
+
+namespace Run.Pug.Resgen.Unity.Editor
+{
+    class ResgenWindow : EditorWindow
+    {
+        [MenuItem("Window/RTool")]
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow<ResgenWindow>();
+        }
+
+        private void OnGUI()
+        {
+        }
+    }
+}
